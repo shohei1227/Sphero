418eb6347f1fae834013a46a71761c11d883fd2b
shohei <71695075+shohei1227@users.noreply.github.com>
2020-11-11 19:21:29 +0900

    Update sphero_Numer0n.js

--- sphero_Numer0n.js
+++ sphero_Numer0n.js
@@ -1,5 +1,5 @@
-function setMode(){
-    let num = 0;
+async function setMode(){
+    let num = 10002;
 	mode = ["easy", "normal", "hard"];
     // mode = [num = 0,num = 1,num = 2]
     while (true) {
@@ -10,26 +10,22 @@ function setMode(){
 		if (R >= 30) {
 			setMainLed({ r: 255, g: 0, b: 0 });
 			num = num + 1;
-			if (num > 2) {
-				num = num % 3;
-			}
+			num = num % 3;
 			await speak(mode[num]);
 		}else if (R <= -30) {
 			setMainLed({ r: 0, g: 0, b: 255 });
 			num = num - 1;
-			if (num < 0) {
-				num = 3 - (Math.abs(num) % 3);
-			}
+			num = num % 3;
 			await speak(mode[num]);
 		}else if (P > 20 || P < -20){
             setMainLed({ r: 0, g: 0, b: 0 });
-		    await speak(String(mode[num]) + "ですね")
+		    await speak("難易度は" + String(mode[num]) + "ですね")
 			break;
         }
         await delay(0.5);
     }
-    return num;
 }
+
 function setNumber() {
 	a = Math.trunc(Math.random() * 9 + 1);
 	return a;
@@ -38,21 +34,21 @@ function ArrayNum(p){　//これだけ後半部分でも使う。
     let x = new Array(p);
     return x;
 }
-function NumberNum(q){
-    let number = ArrayNum(q);
+function NumberNum(p){
+    let number = ArrayNum(p);
     //解答の数字
-	for (i = 0; i < q; i++) {
+	for (i = 0; i < p; i++) {
 		number[i] = setNumber();
     }
     return number;  
 }
-function JudgeNumber(m){
+function JudgeNumber(p){
     var sum1 = 0;
     var sum2 = 0;
-    for (j=0; j < m-1; j++){
-        for (k = j; k < m; k++){
+    for (j=0; j < p-1; j++){
+        for (k = j+1; k < p; k++){
             sum1 += 1;
-            if (num[j] != num[k]){
+            if (number[j] != number[k]){
                 sum2 += 1;
             }
         }
@@ -63,8 +59,8 @@ function JudgeNumber(m){
 //　↑　↑　↑　spheroが考える値の決定に必要な関数
 
 //ここからプレイヤーが数を決定する関数
-function getNumber(){
-    let getNum = 0;
+async function getNumber(){
+    let getNum = 90000;
     ansnum = [1,2,3,4,5,6,7,8,9];
     while (true) {
         P = getOrientation().pitch;
@@ -74,25 +70,21 @@ function getNumber(){
         if (R >= 30) {
             setMainLed({ r: 255, g: 0, b: 0 });
             getNum = getNum + 1;
-            if (getNum > 8) {
-                getNum = getNum % 9;
-            }
-            await speak(ansnum[getNum]);
+            getNum = getNum % 9;
+            await speak(String(ansnum[getNum]));
         }else if (R <= -30) {
             setMainLed({ r: 0, g: 0, b: 255 });
             getNum = getNum - 1;
-            if (getNum < 0) {
-                getNum = 9 - (Math.abs(getNum) % 8);
-            }
-            await speak(mode[getNum]);
+            getNum = getNum % 9;
+            await speak(String(ansnum[getNum]));
         }else if (P > 20 || P < -20){
+            setMainLed({ r: 0, g: 0, b: 0 });
             await speak(String(ansnum[getNum]) + "ですね")
             break;
         } 
         await delay(0.5);
     }
     gotNum = ansnum[getNum];
-    return gotNum;
 }
 async function Judge(s){
     var ans1 = 0;
@@ -102,7 +94,7 @@ async function Judge(s){
             ans1 += 1;
         }else if(number[i] !== answer[i]){
             for (j = 0; j < s; j++){
-                if (number[i] == ans[j]){
+                if (number[i] == answer[j]){
                     ans2 += 1;
                 }
             }
@@ -129,16 +121,17 @@ async function Judge(s){
 //StartProgram
 
 async function startProgram() {
-	num = setMode();
-    mode = num + 2;
+    await setMode();
+    digit = num + 2;
+    await speak("桁数は" + String(digit));
     //mode == 2 --> easy
     //mode == 3 --> normal
     //mode == 4 --> hard 
     while (true) {
-        num = NumberNum(mode);
+        number = NumberNum(digit);
         //　↑ mode桁の数値をセット
         // 数字が重複していたらループでやり直す
-        sum = JudgeNumber(mode);
+        sum = JudgeNumber(digit);
         if (sum[0] == sum[1]){
             break;
         }
@@ -149,17 +142,19 @@ async function startProgram() {
     var Judged = "False" ;
     var score = 1;
 
-	while(Judged == "True"){
-        let answer = ArrayNum(mode);
+	while(Judged == "False"){
+        let answer = ArrayNum(digit);
         for (x = 0; x < mode; x++){
-            answer[x] = getNumber();
+            await getNumber();
+            answer[x] = gotNum;
         }
         await speak(String(answer) + "で解答します。");
-        await Judge(mode);
+        await Judge(digit);
         score += 1//解答に何回かかかったか
     }
     //ここから成功の場合の処理
     //　↓　↓　↓　↓　↓　↓　↓　↓
-    await speak(score);
-}
+    await speak("スコアは" + score);
 
+
+}
